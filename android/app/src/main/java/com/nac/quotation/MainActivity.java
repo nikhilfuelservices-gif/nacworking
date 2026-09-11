ce06bfbe47ee11c3fb00b0eb13343acc32ff5b8a
nikhilfuelservices-gif <nikhilfuelservices@gmail.com>
2026-09-11 12:05:19 +0530

    Add full-screen WebView activity

---
+++ android/app/src/main/java/com/nac/quotation/MainActivity.java
@@ -0,0 +1,31 @@
+package com.nac.quotation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setLoadWithOverviewMode(false);
+        s.setUseWideViewPort(false);
+        webView.loadUrl("https://nacworking.netlify.app");
+        setContentView(webView);
+    }
+    @Override public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack(); else super.onBackPressed();
+    }
+}
